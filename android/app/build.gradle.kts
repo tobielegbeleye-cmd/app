317fe384aa91ce8f68deeb7d7e782e739b9d49fe
Oluwatobi Elegbeleye <146840189+tobielegbeelegbe@users.noreply.github.com>
2025-11-03 13:57:10 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,10 +5,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-tasks.named('copyFlutterAssetsRelease') {
-    doNotTrackState()
-}
-
 android {
     namespace = "com.example.greyfdr"
     compileSdk = flutter.compileSdkVersion
